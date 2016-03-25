a4deee0940f2a1e4c513991d53f47b308c9a9bb4
suptalentdev <suptalentdev@gmail.com>
2016-03-25 15:33:33 +1100

    Slowly building UI - Deciding on layout. Rough framework in place.

--- js/ethercalc.js
+++ js/ethercalc.js
@@ -1,50 +1,63 @@
 $(document).ready(function(){
-		
+	// Adjusts chevron on collapsables	
+  $('#tables').on('hidden.bs.collapse', toggleChevron);
+  $('#tables').on('shown.bs.collapse', toggleChevron);
+  $('#plots').on('hidden.bs.collapse', toggleChevron);
+  $('#plots').on('shown.bs.collapse', toggleChevron);
 		});
 
+    // Scrape data from other sites.
 		$.getJSON("https://www.etherchain.org/api/basic_stats", function(json) {
 			//Find Difficulty and average time
 		window.difficulty = json.data.stats.difficulty/1e12;
 		window.blockTime = json.data.stats.blockTime;
-		window.hashRate = json.data.stats.hashRate/1e9;
-		$('input[name="networkhashrate"]').val(window.hashRate);	
-		$('input[name="difficulty"]').val(window.difficulty);	
-		$('input[name="blocktime"]').val(window.blockTime);	
+		window.networkHashRate = json.data.stats.hashRate/1e9;
+    // Display only a few decimal places, but calculate with full accuracy
+		$('input[name="networkhashrate"]').val(parseFloat(window.networkHashRate).toFixed(3));	
+		$('input[name="difficulty"]').val(parseFloat(window.difficulty).toFixed(3));	
+		$('input[name="blocktime"]').val(parseFloat(window.blockTime).toFixed(2));	
 
 		}
 		);
 
 		$.getJSON("https://poloniex.com/public?command=returnTicker",function(json) {
 			//Find Ethereum Exchange Rates
-		window.eth_btc = json.BTC_ETH.last;
-		window.btc_usd = json.USDT_BTC.last;
-		window.eth_usd = json.USDT_ETH.last;
+		window.btc_eth = json.BTC_ETH.last;
+		window.usd_btc = json.USDT_BTC.last;
+		window.usd_eth = json.USDT_ETH.last;
 		}
 		);
 
-		$.getJSON("http://api.fixer.io/latest?base=AUD",function(json) {
+		$.getJSON("https://bitpay.com/api/Rates",null,function(json) {
 			//Find Difficulty and average time
-		window.aud_usd = json.rates.USD;
-		window.eth_aud=window.eth_btc*window.btc_usd/window.aud_usd
-		$('input[name="exchange"]').val(window.eth_aud)
+		window.aud_btc = json[6].rate;
+		window.aud_eth=window.aud_btc*window.btc_eth
+		$('input[name="exchange"]').val(parseFloat(window.aud_eth).toFixed(2))
+    window.cur_eth = window.aud_eth;
 		}
 		);
 
 
-		function drawplots() {
-		$.jqplot('chart1', [[3,5,8,3,5,2,12,5]]);
-		}
-
-		//Simple Statistical Functions
+		//////////    Simple Statistical Functions  /////////////////
 		// Probability of at least one block
 		function probability(days,prob_solving_block) {
 			//Average number of blocks in "days" 
 			var blocks_days = 3600*24*days/window.blockTime;
 			var probability = 1 - Math.pow((1 - prob_solving_block),blocks_days);
-		
 			//Turn it into a percent
 			return 100*probability;
 		}
+    //This function will return expected number of days required given a certainty. Eg how many days required to be 95% sure we will solve at least one block
+    function daysGivenProbability(Probability, prob_solving_block){
+      var attempts = Math.log(1-Probability)/Math.log(1-prob_solving_block);
+      return attempts*window.blockTime/3600/24;
+    }
+
+    // Expected Days Given Block
+    function daysGivenBlocks(Blocks, prob_solving_block) {
+      var attempts = Blocks/prob_solving_block;
+      return attempts*window.blockTime/3600/24;
+    }
 
 		//expected number of blocks solved
 		function expectation(days,prob_solving_block) {
@@ -54,7 +67,7 @@ $(document).ready(function(){
 			return expected;
 		}
 
-		// Variance as a function of days
+		// STD as a function of days
 		function variance(days,prob_solving_block) {
 			//average number of blocks in "days" 
 			var blocks_days = 3600*24*days/window.blockTime;
@@ -64,7 +77,7 @@ $(document).ready(function(){
 		}
 	
 		// Functions for Drawing Plots
-    function GenerateStandardGraph(id,title,data,xtitle,ytitle,toolTipHeader,toolTipFormat) {
+    function GenerateProbGraph(id,title,data,xtitle,ytitle,toolTipHeader,toolTipFormat) {
 			$(id).highcharts({
         chart: {
           zoomType: 'x'
@@ -75,8 +88,9 @@ $(document).ready(function(){
         },
         xAxis: {title: { text:  xtitle} },
         yAxis: {
-          title: { text: ytitle
-          },
+          title: { text: ytitle },
+          max: 105,
+          endOnTick: false,
         plotLines: [{
           value: 0,
           width: 1,
@@ -103,9 +117,9 @@ $(document).ready(function(){
 
     
 
-    function GenerateExpectationVarianceGraph(expData,stdData,std2UpperData,std2LowerData,maxPlotValue,minPlotValue) 
+    function GenerateGraphWithVariance(Name,Data,stdData,std2UpperData,std2LowerData,maxPlotValue,minPlotValue) 
 {
-			$('#StdGraph').highcharts({
+			$(Name).highcharts({
         chart: {
           zoomType: 'xy',
         },
@@ -115,7 +129,8 @@ $(document).ready(function(){
         },
         xAxis: {title: { text:  "Days"},
             min:0,
-            max: expData[expData.length-1][0]
+            //Make the graph the width of the data
+            max: Data[Data.length-1][0]
         },
         yAxis: {
           title: { text: "Solved Blocks"},
@@ -145,6 +160,9 @@ $(document).ready(function(){
         credits: {
           enabled: false
         },
+        plotOptions: {
+          series: { stickyTracking: false }
+        },
         series: [
        {
           name: "1 Sigma",
@@ -154,6 +172,7 @@ $(document).ready(function(){
           lineWidth: 0,
           marker: { enabled: false },
           tooltip: {
+            shared: false,
             headerFormat: "<b>64.2%</b> of the time will be here<br>",
             pointFormat: "Day: {point.x:,0.2f} Range: {point.low:0.2f} - {point.high:0.2f}"
           },
@@ -166,6 +185,7 @@ $(document).ready(function(){
           lineWidth: 0,
           marker: { enabled: false },
           tooltip: {
+            shared: false,
             headerFormat: "<b>13.6%</b> of the time will be here<br>",
             pointFormat: "Day: {point.x:,0.2f} Range: {point.low:0.2f} - {point.high:0.2f}"
           },
@@ -179,6 +199,7 @@ $(document).ready(function(){
           fillOpacity: 0.3,
           showInLegend: false,
           tooltip: {
+            shared: false,
             headerFormat: "<b>13.6%</b> of the time will be here<br>",
             pointFormat: "Day: {point.x:,0.2f} Range: {point.low:0.2f} - {point.high:0.2f}"
           },
@@ -192,6 +213,7 @@ $(document).ready(function(){
           fillOpacity: 0.3,
           showInLegend: true,
           tooltip: {
+            shared: false,
             headerFormat: "<b>2.1%</b> of the time will be here<br>",
             pointFormat: "Day: {point.x:,0.2f} Range: {point.low:0.2f} -  \u221E"
           },
@@ -205,6 +227,7 @@ $(document).ready(function(){
           fillOpacity: 0.3,
           showInLegend: false,
           tooltip: {
+            shared: false,
             headerFormat: "<b>2.1%</b> of the time will be here<br>",
             pointFormat: "Day: {point.x:0.2f} Range: {point.low:0.2f} - {point.high:0.2f}"
           },
@@ -214,8 +237,9 @@ $(document).ready(function(){
           type: 'spline',
           lineWidth: 2,
           marker: { enabled: false },
-          data: expData,
+          data: Data,
           tooltip: {
+            shared: false,
             headerFormat: "<b>Expected Blocks (Average)</b><br>",
             pointFormat: "Day: {point.x:0.2f} Expected: {point.y:0.2f}"
           },
@@ -253,12 +277,12 @@ $(document).ready(function(){
 		//Calculate Function
 
 		function Calculate() {
+      //Get Data from forms. But use the global variables which contain better accuracy.
 			var hashrate = $("input[name='hashrate']").val();
-			var networkHashRate = document.forms["Calculator"]["networkhashrate"].value;
-			var difficulty = document.forms["Calculator"]["networkhashrate"].value;
       var daysToCompute = $("input[name='days']").val();
-			var prob_solving_block_network = hashrate/(networkHashRate*1e3); //GH/s	
-			var prob_solving_block_difficulty = hashrate/(difficulty*1e6); //TH	
+			var DataPoints = $("input[name='datapoints']").val() +1; //So that it lands nicely on integer days
+			var prob_solving_block_network = hashrate/(window.networkHashRate*1e3); //GH/s	
+			var prob_solving_block_difficulty = hashrate/(window.difficulty*1e6); //TH	
 			//Set up the arrays
 			var probData = new Array();
 			var expData = new Array();
@@ -272,7 +296,6 @@ $(document).ready(function(){
       var maximumPlotValue = new Array();
       var lowerPlotValue = new Array();
 			// Set number of data points. I'm using a quadratic to sample more points earlier on
-			var DataPoints = $("input[name='datapoints']").val();
 			//Increment to satisfy number of days
       //Quadratic Plotting in Graphs
       //var x = Math.sqrt(daysToCompute)/DataPoints;
@@ -314,15 +337,45 @@ $(document).ready(function(){
       lowerPlotValue.push([dependent,0,Math.max(0,expResult - 2*varResult)]);
 
       }
+
+      //Generate Data for tables and fill them
+      var eth_block = 5;
+      var eth_hour = expectation(0.0416667, prob_solving_block_network)*eth_block;
+      var eth_day = expectation(1, prob_solving_block_network)*eth_block;
+      var eth_week = expectation(7, prob_solving_block_network)*eth_block;
+      var eth_month = expectation(30, prob_solving_block_network)*eth_block;
+
+      // Currency Expectations
+      var cur_hour = eth_hour*window.cur_eth;
+      var cur_day = eth_day*window.cur_eth;
+      var cur_week= eth_week*window.cur_eth;
+      var cur_month= eth_month*window.cur_eth;
+
+      // Fill Tables
+      $('#eth_hour').text(parseFloat(eth_hour).toFixed(2));
+      $('#eth_day').text(parseFloat(eth_day).toFixed(2));
+      $('#eth_week').text(parseFloat(eth_week).toFixed(2));
+      $('#eth_month').text(parseFloat(eth_month).toFixed(2));
+      $('#cur_hour').text(parseFloat(cur_hour).toFixed(2));
+      $('#cur_day').text(parseFloat(cur_day).toFixed(2));
+      $('#cur_week').text(parseFloat(cur_week).toFixed(2));
+      $('#cur_month').text(parseFloat(cur_month).toFixed(2));
+
+      var p95_days = daysGivenProbability(0.95, prob_solving_block_network);
+      $('#95_days').text(parseFloat(p95_days).toFixed(2) + " Days");
+      var exp_day = daysGivenBlocks(1, prob_solving_block_network);
+      $('#exp_day').text(parseFloat(exp_day).toFixed(2) + " Days");
+
+
       //Generate the graphs
       //Generate Probability Graph
-      GenerateStandardGraph('#ProbabilityGraph',"Probability of Solving at Least One Block", probData,"Days","Probability (%)", "<b>{series.name}: {point.y:.2f}% </b><br>" , "Days: {point.x: .2f}");
+      GenerateProbGraph('#ProbabilityGraph',"Probability of Solving at Least One Block", probData,"Days","Probability (%)", "<b>{series.name}: {point.y:.2f}% </b><br>" , "Days: {point.x: .2f}");
       //Generate Expectation Graph 
-      GenerateStandardGraph('#ExpectationGraph',"Expected Number of Solved Blocks", expData,"Days","Solved Blocks", "<b>{series.name}: {point.y:.2f} </b><br>" , "Days: {point.x: .2f}");
+     // GenerateStandardGraph('#ExpectationGraph',"Expected Number of Solved Blocks", expData,"Days","Solved Blocks", "<b>{series.name}: {point.y:.2f} </b><br>" , "Days: {point.x: .2f}");
       // Generate Std Graph
-      GenerateStandardGraph('#VarianceGraph',"Standard Deviation", varData,"Days","Solved Blocks", "<b>{series.name}: {point.y:.2f} </b><br>" , "Days: {point.x: .2f}");
+      //GenerateStandardGraph('#VarianceGraph',"Standard Deviation", varData,"Days","Solved Blocks", "<b>{series.name}: {point.y:.2f} </b><br>" , "Days: {point.x: .2f}");
       //Generate Expectation with Variance Widths
-      GenerateExpectationVarianceGraph(expData,stdData,std2UpperData,std2LowerData,maximumPlotValue,lowerPlotValue);
+      GenerateGraphWithVariance("#StdGraph",expData,stdData,std2UpperData,std2LowerData,maximumPlotValue,lowerPlotValue);
 
 //			GenerateExpectationGraph(expData);
 //			GenerateVarianceGraph(varData);
@@ -331,21 +384,58 @@ $(document).ready(function(){
 		}
 
 
+///// Generic Form Validation and Things ////////////////////
+
 
 
   // Form Validation
-
   function validateForm()
 {
   hashrate = $("input[name='hashrate'").val();
   if (hashrate !== "" && !$.isNumeric(hashrate)) {
-    alert("Your hashrate is required");
+    alert("Your hashrate is required and must be numeric");
     return false;
   }
-  // Plot requirements
-  //if (typeof($("input[name='exchange']").val) != number) ||  (typeof($("input[name='exchange']").val) != number) 
-  
+  if (!$.isNumeric($("input[name='exchange']").val())) {
+    alert("Currency is required and must be numeric");
+    return false;
+  }
+
   return true;
+}
+
+  // Currency Swap
+  function CurrencyChange()
+  {
+		if($('input:checked').val() == "AUD"){
+		  $('input[name="exchange"]').val(parseFloat(window.aud_eth).toFixed(2));
+      window.cur_eth = aud_eth;
+    }
+    else
+    {
+		  $('input[name="exchange"]').val(parseFloat(window.usd_eth).toFixed(2));
+      window.cur_eth = usd_eth;
+    }
+  }
+
+  // If user manually updates currency update global variables
+  function UpdateCurrency()
+  {
+		if($('input:checked').val() == "AUD"){
+		  window.aud_eth = $('input[name="exchange"]').val();
+    }
+    else
+    {
+		  window.usd_eth = $('input[name="exchange"]').val();
+    }
+  }
+
+// Collapse Chevron Functions
+function toggleChevron(e) {
+      $(e.target)
+        .prev('.panel-heading')
+        .find("i.indicator")
+        .toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
 }
 
 
