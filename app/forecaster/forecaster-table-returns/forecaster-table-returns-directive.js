c0bdca6e1708cc7593d51c386be040a4e8ea0880
suptalentdev <suptalentdev@gmail.com>
2016-07-08 20:09:09 +1000

    Moved the forecaster tables into individual directives

---
+++ app/forecaster/forecaster-table-returns/forecaster-table-returns-directive.js
@@ -0,0 +1,7 @@
+angular.module('ethMiningCalc')
+  .directive('forecasterTableReturns', function() {
+    return {
+        restrict: 'AE',
+        templateUrl: 'app/forecaster/forecaster-table-returns/forecaster-table-returns.html',
+    };
+  });
---
+++ app/forecaster/forecaster-table-solution-stats/forecaster-table-solution-stats-directive.js
@@ -0,0 +1,7 @@
+angular.module('ethMiningCalc')
+  .directive('forecasterTableSolutionStats', function() {
+    return {
+        restrict: 'AE',
+        templateUrl: 'app/forecaster/forecaster-table-solution-stats/forecaster-table-solution-stats.html',
+    };
+  });
